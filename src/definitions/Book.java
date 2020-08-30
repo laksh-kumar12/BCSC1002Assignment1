697875611dc8b0f1af9f2c8e99f030941e9a8f00
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2020-08-30 12:18:09 +0530

    create the toString method for Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -11,33 +11,42 @@ public class Book {
     private String authorName;
     private String isbnNumber;
 
+    public Book(String bookName, String authorName, String isbnNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.isbnNumber = isbnNumber;
+    }
+
     public String getBookName() {
         return bookName;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public String getIsbnNumber() {
-        return isbnNumber;
     }
 
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
 
+    public String getAuthorName() {
+        return authorName;
+    }
+
     public void setAuthorName(String authorName) {
         this.authorName = authorName;
+    }
+
+    public String getIsbnNumber() {
+        return isbnNumber;
     }
 
     public void setIsbnNumber(String isbnNumber) {
         this.isbnNumber = isbnNumber;
     }
 
-    public Book(String bookName, String authorName, String isbnNumber) {
-        this.bookName = bookName;
-        this.authorName = authorName;
-        this.isbnNumber = isbnNumber;
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", authorName='" + authorName + '\'' +
+                ", isbnNumber='" + isbnNumber + '\'' +
+                '}';
     }
 }
